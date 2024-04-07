2bd0dcbb9c3e67929d5e72adf17037af7a94e2cf
pigchampmonitoring <166279981+pigchampmonitoring@users.noreply.github.com>
2024-04-07 18:26:35 +0800

    Update script.js

--- script.js
+++ script.js
@@ -108,17 +108,18 @@ function registerUser(event) {
       return response.text();
     })
     .then((data) => {
-      console.log("Success:");
-      if (data !== "Data is not present") {
-        // Token is received
-        console.log("Token:", data);
-        localStorage.setItem("token", data);
-        window.location.href = "Pigs-Profile.html";
-    }
-    });
-    .catch((error) => {
-      console.error("Error:", error);
-    });
+  console.log("Success:");
+  if (data !== "Data is not present") {
+    // Token is received
+    console.log("Token:", data);
+    localStorage.setItem("token", data);
+    window.location.href = "Pigs-Profile.html";
+  }
+})
+.catch((error) => {
+  console.error("Error:", error);
+});
+
 // Clear input fields immediately after form submission
   document.querySelector(
     '.registration-form input[name="username"]'
